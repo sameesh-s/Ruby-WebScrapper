1c957669135bdd6e6f0ec26af48e0940425277e8
sameesh-s <sameesh.fisat@gmail.com>
2016-08-13 11:27:35 +0530

    text strip' text striping is done at the time of mapping

--- app.rb
+++ app.rb
@@ -15,12 +15,12 @@ require 'json'
   post '/result' do
     doc = Nokogiri::HTML open (params[:title].to_s)
     links = doc.css("a").map{ |i| i["href"]}
-    heading = doc.css("h1,h2,h3,h4").map{|i| i}
+    heading = doc.css("h1,h2,h3,h4").map{|i| [i.name,i.text.strip]}
     res = {}
-    res['h1']=heading.select{|i| i.name=="h1"}.map{|i| i.text.strip}
-    res['h2']=heading.select{|i| i.name=="h2"}.map{|i| i.text.strip}
-    res['h3']=heading.select{|i| i.name=="h3"}.map{|i| i.text.strip}
-    res['h4']=heading.select{|i| i.name=="h4"}.map{|i| i.text.strip}
+    res['h1']=heading.select{|i| i[0]=="h1"}.map{|i| i[1]}
+    res['h2']=heading.select{|i| i[0]=="h2"}.map{|i| i[1]}
+    res['h3']=heading.select{|i| i[0]=="h3"}.map{|i| i[1]}
+    res['h4']=heading.select{|i| i[0]=="h4"}.map{|i| i[1]}
     res['links']=links.select{|i| i[0]=='h'}
     content_type :json
       JSON.pretty_generate res
